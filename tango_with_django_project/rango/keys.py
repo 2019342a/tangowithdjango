55f5ced7183b88f0b9cb9ebb926901b3f251bf40
Theofilos Alexiou <theofilosalexiou@gmail.com>
2015-03-07 15:12:30 +0000

    Chapter 15-completed

---
+++ tango_with_django_project/rango/keys.py
@@ -0,0 +1,3 @@
+__author__ = 'Theofilos Alexiou'
+
+BING_API_KEY = ''
--- tango_with_django_project/rango/urls.py
+++ tango_with_django_project/rango/urls.py
@@ -1,4 +1,4 @@
-__author__ = 'user'
+__author__ = 'Theofilos Alexiou'
 from django.conf.urls import patterns, url
 from rango import views
 
@@ -12,4 +12,5 @@ urlpatterns = patterns('',
         url(r'^login/$', views.user_login, name='login'),
         url(r'^restricted/', views.restricted, name='restricted'),
         url(r'^logout/$', views.user_logout, name='logout'),
+        url(r'^search/$', views.search, name='search'),
         )
